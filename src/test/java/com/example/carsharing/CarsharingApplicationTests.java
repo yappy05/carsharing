659b5051871f0f8f038185b4f2b4be3558bdaa13
sergey <wendingnn@gmail.com>
2025-06-15 21:41:05 +0300

    test

--- src/test/java/com/example/carsharing/CarsharingApplicationTests.java
+++ src/test/java/com/example/carsharing/CarsharingApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.carsharing;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 @SpringBootTest
 class CarsharingApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertNotEquals(1, 1, "1 не должно быть равно 0");
 	}
 
 }
